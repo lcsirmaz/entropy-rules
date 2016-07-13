78bda1bba616ef02c624f5c6b39235aaef489a0e
Laszlo Csirmaz <csirmaz@degas.ceu.hu>
2016-07-13 12:18:51 +0200

    New inequalities, mainly from rules

--- utils/downgrade.pl
+++ utils/downgrade.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl -w
 ##
-## check a file of optional new inequalities
+## check a result file for new inequalities. Truncate large coeffs.
 ##
-## arguments: <result> <file1> <file2> ...
+## arguments: [-t <threshold>] <result> <file1> <file2> ...
 ##
+##   <threshold> is the maximal value of (normalized) coeffs.
 ##   <result> is a file where each checked line looks like this:
 ## V 1/2 7/6 5/6 1/2 3/2 1/6 0 0 0 0
 ##    these are natural coeffs 2..11, the Ingleton coeff is 1
@@ -17,22 +18,22 @@
 
 use strict;
 
+# threshold value, start of <ineq?> arguments
+my $ths=200; my $ARGZ=0;
+
 sub print_usage {
-    print "Usage: checkall.pl <result> <ineq1> <ineq2> ...\n",
-          "   <result> contains vertices to be checked\n",
+    print "Usage: downgrade.pl [-s <supd>] [-t <threshold>] <result> <ineq1> <ineq2> ...\n",
+          "   <supd> is the list of superseded inequalities (speeds up checking)\n",
+          "   <threshold> is the maximal valur for (integer) coeffs\n",
+          "   <result> contains the vertices to be checked\n",
           "   <ineq?> files contain known entropy inequalities\n",
           "The result file should be XXXX.res, and the first line\n",
           "of XXXX.vlp should contain the copy string. The label\n",
-          "is the basename of the result file.\n";
+          "is the basename of the result file. Rounded coeffs are\n",
+          "marked by an *\n";
     exit 1;
 }
-
-if(scalar @ARGV < 2){ print_usage(); }
-if( $ARGV[0] !~ /\.res$/ ){
-    die "First argument should be a result file ending with .res\n";
-}
-
-####################################################################
+###################################################################
 ## compute the lexicographically maximal version
 sub swap_ab { # 2<->3, 5<->6, 7<->8
     my $a=shift; my $t;
@@ -80,6 +81,53 @@ sub lcm {
     return $a*int($b/gcd($a,$b)+0.01);
 }
 ###############################################################
+## round down the coefficients
+## find out the multiplier which gives the smallest L1 norm
+sub comp_l1{
+    my($i,$arr)=@_;
+    my $v=0.0; my $coeff=($i+0.0)/($arr->[0]+0.0);
+    for my $i(1..10){
+        my $s=$arr->[$i]*$coeff;
+        $s = int($s+1.0-1e-7) - $s; ## round up
+        if($s<-1e-7) { $s+=1.0; } elsif($s<0){$s=0;}
+        $v+=$s;
+    }
+    return $v/$i;
+}
+
+## downgrade
+sub downgrade { # $arr[0..10], $new[0..10]
+    my($arr,$new)=@_;
+    my $max=$arr->[0];
+    for my $i(1..10){ my $v=$arr->[$i]; $max=$v if($max<$v); }
+    my $upto = int(1e-7+($max+0.0)/($ths+0.0));
+    return 0 if($upto<1); ## cannot downgrade
+    my $L1=1000.0; my $ing=0;
+    for my $i(1..$upto){
+        my $newl1=comp_l1($i,$arr);
+        if($ing<1 || $newl1<$L1){$ing=$i; $L1=$newl1; }
+    }
+    $new->[0]=$ing; my $cf=($ing+0.0)/$arr->[0];
+    for my $i(1..10){
+        my $s=$arr->[$i]*$cf;
+        my $is=int($s+1.0-1e-7); if($is-$s<-1e-7){$is++;}
+        $new->[$i]=$is;
+    }
+    # check if the downgraded inequality is superseded by one of the known ones
+    # 1, 1,1,1, 0,0,0, 0,0,0,0 (ZY)
+    return 0 if($new->[1]>=$new->[0] && $new->[2]>=$new->[0] && $new->[3]>=$new->[0]);
+    # 2, 1,3,2, 0,0,0, 0,0,0,0
+    return 0 if(2*$new->[1]>=$new->[0] && 2*$new->[2]>=3*$new->[0] && $new->[3]>=$new->[0]);
+    # 2, 3,2,1, 0,0,0, 0,0,0,0
+    return 0 if(2*$new->[1]>=3*$new->[0] && $new->[2]>=$new->[0] && 2*$new->[3]>=$new->[0]);
+    # 3, 1,5,5, 0,0,0, 0,0,0,0
+    return 0 if(3*$new->[1]>=$new->[0] && 3*$new->[2]>=5*$new->[0] && 3*$new->[3]>=5*$new->[0]);
+    # 3, 5,5,1, 0,0,0, 0,0,0,0
+    return 0 if(3*$new->[1]>=5*$new->[0] && 3*$new->[2]>=5*$new->[0] && 3*$new->[3]>=$new->[0]);
+    return 1;
+}
+
+###############################################################
 ## read in the result file
 sub lexmin {
     my($a,$b)=@_;
@@ -104,31 +152,49 @@ sub read_result_file {
       for my $i(1..10){
           if($v[$i] =~ /^\d+\/(\d+)$/) { $d=lcm($d,$1); }
       }
-      my @a=(); $a[0]=$d;
+      my @a=(); $a[0]=$d; my $downgrade=$d>$ths ? 1 : 0;
       for my $i(1..10){
           if($v[$i] =~ /^\d+$/ ){ $a[$i]=$d*$v[$i]; }
           elsif( $v[$i] =~ /^(\d+)\/(\d+)$/ ){
               $a[$i]=int($1*$d/$2+0.01);
+              $downgrade=1 if($a[$i]>$ths);
           } else {
-# skip these lines, they would give too large coeffs anymore
-##             print "wrong line in $fname:\n   $line\n";
-             $a[0]=-1;
+             $downgrade=1;
+             $a[$i]=$d*(0.0+$v[$i]);
           }
       }
-      next if($a[0]<0);
-# check if some entry is >999, if yes then skip this line ...
-      my $skipit=0;
-      for my $i(1..10){ $skipit=1 if($a[$i]>999); }
-      next if($skipit);
-      biggest(\@a);    ## make it lexicographically maximal
-      push @lines, \@a;
+      biggest(\@a);  ## make it lexicographically maximal
+      if($downgrade){
+          my $new=[];
+          next if(!downgrade(\@a,$new));
+          $new->[11]="*";
+          push @lines, $new;
+      } else {
+          push @lines, \@a;
+      }
     }
     close(FILE);
     foreach my $a (sort {lexmin($a,$b)} @lines ){
       push @{$info->{new}}, $a;  ## and store it
     }
 }
+
 ## read known inequalities
+my %supplist=();
+
+sub read_supplist {
+    my $file=shift;
+    open(FILE,$file) || die "Cannot open supserseded file $file\n";
+    my $cnt=0;
+    while(<FILE>){
+        next if(!/^superseded: [^\s]+\/([^\s]+) by/);
+        $supplist{$1}=1;
+        $cnt++;
+    }
+    close(FILE);
+    die "No superseded items found in $file\n" if($cnt==0);
+}
+
 sub read_ineq_file {
     my ($info,$fname)=@_;
     $info->{old}=() if(!defined $info->{old});
@@ -138,11 +204,14 @@ sub read_ineq_file {
       chomp; my $line=$_;
       my @a=split(/,\s*/,$line);
       scalar @a>=11 || die "Wrong data line in inequality file $fname:\n  $line\n";
+      next if(defined $a[13]); ## 'S'
+      next if(defined $a[12] && defined $supplist{$a[12]});
       biggest(\@a);    ## make it lexocographically maximal
       push @{$info->{old}}, \@a; ## and store
     }
     close(FILE);
 }
+
 ################################################################
 ## generate all relevant permutation of inequalities
 sub not_the_same {
@@ -263,24 +332,41 @@ sub find_copy {
         my $a=<FILE>; close(FILE);
         chomp($a);
         if($a=~/^c copy/){
-            $a=~ s/^.*string: //; $info->{copy}="$a, copy:";
+            $a=~ s/^.*string: //; $info->{copy}="$a"; $info->{id}="copy:";
         } elsif( $a=~/rule (\d.+) for/){
-            $info->{copy} = "rule$1, rule:";
+            $info->{copy} = "rule$1"; $info->{id}="rule:";
         }
     }
     $fname =~ /([a-z\d]+)\.vlp$/;
-    $info->{id} = $1;
+    $info->{id} .= $1;
 }
 ################################################################
 ##
 my $info={};
 
-for my $i(1..-1+scalar @ARGV){
+if(scalar @ARGV < 2){ print_usage(); }
+if($ARGV[0] eq "-s"){
+    if(scalar @ARGV<4){ print_usage(); }
+    read_supplist($ARGV[1]);
+    $ARGZ=2;
+}
+if($ARGV[$ARGZ] eq "-t"){
+    if(scalar @ARGV<4+$ARGZ || $ARGV[$ARGZ+1] !~ /^\d+$/){ print_usage(); }
+    $ths=0+$ARGV[$ARGZ+1]; if($ths<10||$ths>1000){ print_usage();}
+    $ARGZ+=2;
+}
+if($ARGV[$ARGZ] !~ /\.res$/ ){
+    die "First argument should be a result file endign with .res\n";
+}
+
+
+
+for my $i($ARGZ+1..-1+scalar @ARGV){
     read_ineq_file($info,$ARGV[$i]);
 }
-read_result_file($info,$ARGV[0]);
+read_result_file($info,$ARGV[$ARGZ]);
 
-find_copy($info,$ARGV[0]);
+find_copy($info,$ARGV[$ARGZ]);
 
 ## print "old: ",scalar @{$info->{old}},", new: ",scalar @{$info->{new}},"\n";
 generate_matrix($info);
@@ -299,7 +385,7 @@ foreach my $a(@{$info->{new}}){
     for my $i(0..10){
         print $a->[$i],","; print " " if($i==0 || $i==3 || $i==6);
     }
-    print " ",$info->{copy},$info->{id},":$cnt\n";
+    print " ",$info->{copy},", ",($a->[11]?"*":""),$info->{id},":$cnt\n";
     # store it, and use it subsequently
     add_to_matrix($info,$a);
 }
